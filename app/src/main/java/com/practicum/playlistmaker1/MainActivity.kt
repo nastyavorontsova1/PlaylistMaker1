213abc8aa61e7f1aa54ec2d1095c2fcf94301263
Anastasia <vorontsovanastasia@yandex.ru>
2024-10-06 17:52:48 +0300

    Домашнее задание по 8 спринту

--- app/src/main/java/com/practicum/playlistmaker1/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker1/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.practicum.playlistmaker1
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.practicum.playlistmaker1.R
+import com.practicum.playlistmaker1.SearchActivity
+import com.practicum.playlistmaker1.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonSearchMain = findViewById<Button>(R.id.search_main)
+        val buttonMediaMain = findViewById<Button>(R.id.media_main)
+        val buttonSettingsMain = findViewById<Button>(R.id.settings_main)
+
+        val searchIntent = Intent(this, SearchActivity::class.java)
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startActivity(searchIntent)
+            }
+        }
+
+        buttonSearchMain.setOnClickListener(buttonClickListener)
+
+        buttonMediaMain.setOnClickListener {
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+        buttonSettingsMain.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
     }
 }
